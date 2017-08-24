9e0fb80773ddbfcace2d847e2c46b562211e956a
Kamil Korolkiewicz <kamil.korolkiewicz@pragmasoft.pl>
2017-08-24 10:14:52 +0200

    Moving original Expr codes. JS keys update

--- System/CSys.ts
+++ System/CSys.ts
@@ -121,14 +121,10 @@ module ExprAE.System {
             }
         }
 
-        static KeyPressed(code: number): boolean {
+        static KeyPressed(code: Keys): boolean {
             var keynum;
             
-            if(window.event) { // IE                    
-                keynum = e.keyCode;
-            } else if(e.which){ // Netscape/Firefox/Opera                   
-                keynum = e.which;
-            }
+            //todo get keynum
 
             return keynum==code;
         }
